1ad71d81543f8208b12153be3d426f9940e368ae
Martín <martin.g.lopez@gmail.com>
2025-08-03 18:20:07 -0300

    Listing details in template

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,11 +4,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing, Bid
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listing.objects.all().order_by('-created_at')
+    current_bids = Bid.objects.all()
+    return render(request, "auctions/index.html", {
+        "listings" : listings,
+        "current_bids" : current_bids
+    })
 
 
 def login_view(request):
