858e9f820c0da33588dad792bf039c843f1994c5
Martín <martin.g.lopez@gmail.com>
2025-08-04 10:48:50 -0300

    Added created_ad ordering for listings in index view

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -11,7 +11,7 @@ from .models import User, Listing, Bid
 def index(request):
     # listings = Listing.objects.all().order_by('-created_at')
     # current_bids = Bid.objects.all() # USAR MAX PARA SACAR EL BID MAS ALTO
-    listings = Listing.objects.annotate(max_bid=Max("bids__ammount"))
+    listings = Listing.objects.annotate(max_bid=Max("bids__ammount")).order_by('-created_at')
     return render(request, "auctions/index.html", {
         "listings" : listings
     })
