582c7b8c9187d89e92aed7b2b50d02175a7a5845
Martín <martin.g.lopez@gmail.com>
2025-08-04 10:33:07 -0300

    Used aggregation to get the bid with the max ammount for each listing in the index view

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -9,11 +9,11 @@ from .models import User, Listing, Bid
 
 
 def index(request):
-    listings = Listing.objects.all().order_by('-created_at')
-    current_bids = Bid.objects.all() # USAR MAX PARA SACAR EL BID MAS ALTO
+    # listings = Listing.objects.all().order_by('-created_at')
+    # current_bids = Bid.objects.all() # USAR MAX PARA SACAR EL BID MAS ALTO
+    listings = Listing.objects.annotate(max_bid=Max("bids__ammount"))
     return render(request, "auctions/index.html", {
-        "listings" : listings,
-        "current_bids" : current_bids
+        "listings" : listings
     })
 
 
